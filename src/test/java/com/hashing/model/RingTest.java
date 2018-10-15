e26af2baf07567c7120e7829c34c989ff00066d4
Miguel Gomes <jmbg.41@gmail.com>
2018-10-15 08:59:52 +0100

    Remove Main

---
+++ src/test/java/com/hashing/model/RingTest.java
@@ -0,0 +1,36 @@
+package com.hashing.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class RingTest {
+
+  @Test
+  public void testRing() {
+
+    List<String> requests = Arrays.asList(
+        "test",
+        "test2",
+        "adasdadsasd",
+        "kkkkk",
+        "olsslsoslo"
+    );
+
+    Node node = new Node("192.168.1.1");
+
+    Node node1 = new Node("192.168.10.1");
+
+    Ring ring = new Ring();
+    ring.addNode(node);
+    ring.addNode(node1);
+
+    requests.forEach(ring::addValue);
+
+    ring.printRing();
+
+  }
+
+}
